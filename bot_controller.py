bb444ab967fcb0c57a4ac7a80debed8bea3a81d4
Peter Bulloch <pbulloch123@gmail.com>
2023-09-13 17:08:57 -0600

    added pagination if over 2000 characters (discord limit). added error handling. added parsing for commands and added escape character \ in front

--- bot_controller.py
+++ bot_controller.py
@@ -2,6 +2,10 @@ import discord
 from discord.ext import commands
 from text_by_api import get_response
 import os
+import logging
+
+# debug logging
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 # load environment variables from .env file
 from dotenv import load_dotenv
@@ -35,10 +39,37 @@ async def on_message(message):
     if message.content.startswith('!ask'):
         prompt = message.content[5:].strip()
         response = get_response(prompt)
-        # Extract only the content message from the API response
+        # extract only the content message from the API response
         api_content = response["content"]
-        await message.channel.send(api_content)
+        if len(api_content) >= 2000:
+            await send_paginated_message(message.channel, api_content)
+        else:
+            # Otherwise, send the response as-is
+            await message.channel.send(api_content)
+    
+# function to send a paginated message
+async def send_paginated_message(channel, text):
+    max_chars = 2000  # discord's maximum message length
+    start = 0
+
+    while start < len(text):
+        end = start + max_chars
+        if end > len(text):
+            end = len(text)  # ensure end doesn't go beyond the text length
+
+        # escape / and > characters before sending
+        chunk = text[start:end]
+        chunk = chunk.replace('/', '\/')  # replace / with \/
+        chunk = chunk.replace('>', '\>')  # replace > with \>
+
+        if text[end:end + 1] == '\0':  # check for a null character
+            await channel.send(chunk)
+            return
+        else:
+            await channel.send(chunk)
+            start = end
+
 
 # run the bot with your Discord bot token
-bot.run(f"{DISCORD_BOT_TOKEN}")
+bot.run(f"{DISCORD_BOT_TOKEN}", log_handler=handler)
 
