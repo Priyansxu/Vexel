0d8c11c6adb5b0ef3f695fb5a955ceedc6d8abfc
Peter Bulloch <pbulloch123@gmail.com>
2023-09-13 15:58:56 -0600

    added intents. bot now works with discord and chatgpt api

--- bot_controller.py
+++ bot_controller.py
@@ -10,8 +10,14 @@ load_dotenv()
 # access the discord bot token environment variable
 DISCORD_BOT_TOKEN = os.getenv("YOUR_DISCORD_BOT_TOKEN")
 
+# define the intents for your bot
+intents = discord.Intents.default()
+intents.typing = False  # You can adjust these based on your bot's needs
+intents.presences = False
+intents.message_content = True  # Enable message content intent
+
 # initialize the Discord bot
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # OPTIONAL -- define the channel ID where the bot should respond. 
 # Otherwise, it will respond to all channels.
@@ -29,8 +35,10 @@ async def on_message(message):
     if message.content.startswith('!ask'):
         prompt = message.content[5:].strip()
         response = get_response(prompt)
-        await message.channel.send(response)
+        # Extract only the content message from the API response
+        api_content = response["content"]
+        await message.channel.send(api_content)
 
 # run the bot with your Discord bot token
-bot.run("DISCORD_BOT_TOKEN")
+bot.run(f"{DISCORD_BOT_TOKEN}")
 
