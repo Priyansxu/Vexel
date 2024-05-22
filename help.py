c11b1ea6db82e85d4c64aa7f7c6c758074eedd72
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-05-22 23:18:28 +0530

    Update help.py

--- help.py
+++ help.py
@@ -2,9 +2,9 @@ async def help_command(message):
     HELP_TEXT = """
     > Hey there! I'm Vexel, an AI assistant discord bot!
 
-   *COMMANDS:*
-   - `!ask ` `prompt ` - Ask a question for any information.
-   - `!draw ` ` prompt ` - Request to generate an image based on your prompt.
+   *Commands:*
+  - `!ask ` `prompt ` - Ask a question for any information.
+  - `!draw ` ` prompt ` - Request to generate an image based on your prompt.
     """ 
     
     await message.channel.send(HELP_TEXT) 
