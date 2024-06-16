121f6490fedf54e125c8b2d0123792947169c9e3
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-06-16 13:22:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,21 @@
 import platform
-import os
 import logging
 import discord
+import os
 from discord.ext import commands
 from dotenv import load_dotenv
 from helpers.logger import setup_logger
 
-# Load environment variables
 load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
-# Logging setup
 discord_logger = setup_logger('discord', 'discord.log', level=logging.DEBUG)
 server_logger = setup_logger('server', 'server.log')
 chat_logger = setup_logger('chat', 'chats.log')
 
 class Vexel(commands.Bot):
     def __init__(self, *args, **kwargs):
-        super().__init__( *args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.logger = discord_logger
         self.server_logger = server_logger
         self.chat_logger = chat_logger
@@ -34,8 +32,7 @@ class Vexel(commands.Bot):
         for filename in os.listdir("./cogs"):
             if filename.endswith(".py"):
                 await self.load_extension(f"cogs.{filename[:-3]}")
-                self.logger.info(f"Successfully loaded the {filename} cog!")
-
+                self.logger.info(f"Successfully loaded the {filename}!")
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -43,13 +40,12 @@ intents.presences = False
 intents.message_content = True
 
 bot = Vexel(
-    command_prefix=commands.when_mentioned_or("!"), # The prefix for commands
-    case_insensitive=True, # Whether the commands are case insensitive
-    intents=intents, # The intents for the bot to receive events
-    help_command=None, # Disable the default help command
-    activity=discord.Activity(type=discord.ActivityType.listening, name="!help"), # The activity for the bot to display
-    status=discord.Status.idle # The status for the bot to display
-  )
+    command_prefix=commands.when_mentioned_or("!"),
+    case_insensitive=True,
+    intents=intents,
+    help_command=None,
+    activity=discord.Activity(type=discord.ActivityType.listening, name="@vexel"),
+    status=discord.Status.idle
+)
 
-# Run bot
-bot.run(DISCORD_BOT_TOKEN)
+bot.run(DISCORD_BOT_TOKEN) 
