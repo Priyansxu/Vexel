5759e534abd169000515102f9893583d15430906
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-06-16 13:41:56 +0530

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -9,16 +9,15 @@ class Events(commands.Cog):
         self.chat_logger = bot.chat_logger
         self.server_logger = bot.server_logger
         self.conversation_histories = bot.conversation_histories
-    
-    # Guild join event
+
     @commands.Cog.listener()
     async def on_guild_join(self, guild) -> None:
         server_name = guild.name
         server_owner = await guild.fetch_member(guild.owner_id)
         total_members = guild.member_count
         self.server_logger.info(f"Joined new server: {server_name}, Owner: {server_owner}, Total Members: {total_members}")
-    
-        channel_id = 1243869783406018640  # Adjust this channel ID as needed
+
+        channel_id = 1243869783406018640
         channel = self.bot.get_channel(channel_id)
         message = (
             f"Joined new server:\n"
@@ -27,21 +26,23 @@ class Events(commands.Cog):
             f"**Total Members:** {total_members}"
         )
         await channel.send(message)
-     
-    # Message listener
+
     @commands.Cog.listener()
     async def on_message(self, message) -> None:
         if message.author.bot:
             return
-        
+
         user_id = str(message.author.id)
         if user_id not in self.conversation_histories:
             self.conversation_histories[user_id] = []
-    
+
         user_prompt = message.content
         username = message.author.name
-    
+
         if self.bot.user.mentioned_in(message):
+            if "@everyone" in message.content or "@here" in message.content:
+                return
+
             prompt = message.content.replace(f'<@{self.bot.user.id}>', '').strip()
             if prompt:
                 self.conversation_histories[user_id].append({"role": "user", "content": prompt})
@@ -55,11 +56,11 @@ class Events(commands.Cog):
                         else:
                             await message.reply(response)
                     else:
-                        await message.reply("Sorry, I couldn't answer you right now. You can use !draw cmnd for now! Thanku")
+                        await message.reply("Sorry, I couldn't answer you right now.")
             else:
                 await message.reply("Hey there, how can I assist you today? For commands, execute !help")
             return
-          
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Events(bot))
+     
