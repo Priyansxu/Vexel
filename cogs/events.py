09a1ea56ff230526db670d134a29603c74cddbab
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-06-17 11:50:14 +0530

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -1,66 +1,28 @@
 import discord
 from discord.ext import commands
-from helpers.pagination import send_paginated_message
-from helpers.ai import get_response
 
 class Events(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.chat_logger = bot.chat_logger
         self.server_logger = bot.server_logger
-        self.conversation_histories = bot.conversation_histories
 
     @commands.Cog.listener()
-    async def on_guild_join(self, guild) -> None:
+    async def on_guild_join(self, guild: discord.Guild) -> None:
         server_name = guild.name
-        server_owner = await guild.fetch_member(guild.owner_id)
+        server_owner = guild.owner
         total_members = guild.member_count
         self.server_logger.info(f"Joined new server: {server_name}, Owner: {server_owner}, Total Members: {total_members}")
 
         channel_id = 1243869783406018640
         channel = self.bot.get_channel(channel_id)
-        message = (
-            f"Joined new server:\n"
-            f"**Server Name:** {server_name}\n"
-            f"**Owner:** {server_owner}\n"
-            f"**Total Members:** {total_members}"
-        )
-        await channel.send(message)
-
-    @commands.Cog.listener()
-    async def on_message(self, message) -> None:
-        if message.author.bot:
-            return
-
-        user_id = str(message.author.id)
-        if user_id not in self.conversation_histories:
-            self.conversation_histories[user_id] = []
-
-        user_prompt = message.content
-        username = message.author.name
-
-        if self.bot.user.mentioned_in(message):
-            if "@everyone" in message.content or "@here" in message.content:
-                return
-
-            prompt = message.content.replace(f'<@{self.bot.user.id}>', '').strip()
-            if prompt:
-                self.conversation_histories[user_id].append({"role": "user", "content": prompt})
-                async with message.channel.typing():
-                    response = get_response(self.conversation_histories[user_id])
-                    self.chat_logger.info(f"{username}: {prompt}\nVexel: {response}\n")
-                    if response:
-                        self.conversation_histories[user_id].append({"role": "assistant", "content": response})
-                        if len(response) >= 2000:
-                            await send_paginated_message(message.channel, response)
-                        else:
-                            await message.reply(response)
-                    else:
-                        await message.reply("Sorry, I couldn't answer you right now.")
-            else:
-                await message.reply("Hey there, how can I assist you today? For commands, execute !help")
-            return
+        if channel:
+            message = (
+                f"Joined new server:\n"
+                f"**Server Name:** {server_name}\n"
+                f"**Owner:** {server_owner}\n"
+                f"**Total Members:** {total_members}"
+            )
+            await channel.send(message)
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(Events(bot))
-     
+    await bot.add_cog(Events(bot)) 
