47159545140b940f24c4aeefe94c917ac8b7e28f
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-06-24 12:32:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,40 +4,47 @@ import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
-from helpers.logger import setup_logger
 
 load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
-discord_logger = setup_logger('discord', 'discord.log', level=logging.DEBUG)
-server_logger = setup_logger('server', 'server.log')
-chat_logger = setup_logger('chat', 'chats.log')
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+handler.setFormatter(formatter)
+if not logger.hasHandlers():
+    logger.addHandler(handler)
 
 class Vexel(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.logger = discord_logger
-        self.server_logger = server_logger
-        self.chat_logger = chat_logger
         self.conversation_histories = {}
   
     async def on_ready(self) -> None:
-        self.logger.info(f"{self.user} is now online!")
-        self.logger.info(f"Logged in as {self.user.name}")
-        self.logger.info(f"discord.py API version: {discord.__version__}")
-        self.logger.info(f"Python version: {platform.python_version()}")
-        self.logger.info(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+        logger.info(f"{self.user} is now online!")
+        logger.info(f"Logged in as {self.user.name}")
+        logger.info(f"discord.py API version: {discord.__version__}")
+        logger.info(f"Python version: {platform.python_version()}")
+        logger.info(f"Running on: {platform.system()} {platform.release()} ({os.name})")
     
     async def setup_hook(self) -> None:
         for filename in os.listdir("./cogs"):
             if filename.endswith(".py"):
                 await self.load_extension(f"cogs.{filename[:-3]}")
-                self.logger.info(f"Successfully loaded the {filename}!")
+                logger.info(f"Successfully loaded the {filename}!")
+        await self.tree.sync()
+
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            logger.warning(f"A command not found")
+        else:
+            logger.error(f"An error occurred: {error}")
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
-intents.message_content = True
+intents.message_content = False
 
 bot = Vexel(
     command_prefix=commands.when_mentioned_or("!"),
